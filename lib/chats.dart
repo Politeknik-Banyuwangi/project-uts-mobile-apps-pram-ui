aea5cf7ad01cf359a534ce5dbc89069d36aca52c
FlutLab.io <>
2022-05-31 05:51:59 +0000

    Autosave

--- lib/chats.dart
+++ lib/chats.dart
@@ -56,3 +56,54 @@ class Chats extends StatelessWidget {
     );
   }
 }
+
+Widget build(BuildContext context) {
+  return Scaffold(
+    body: NotificationListener<OverscrollIndicatorNotification>(
+      onNotification: (_) {
+        _.disallowGlow();
+        return true;
+      },
+      child: ListView(
+        children: [
+          SizedBox(
+            height: 10,
+          ),
+
+          // Kodingan dimulai
+          ListTile(
+            leading: CircleAvatar(
+              backgroundImage: NetworkImage("https://i0.wp.com/www.cirebonkota.go.id/wp-content/uploads/2018/05/jokowi.jpg"),
+              radius: 30.0,
+            ),
+            title: Text(
+              'Anda',
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            subtitle: Row(
+              children: [
+                Icon(
+                  Icons.done_all_rounded,
+                  color: Colors.blue,
+                ),
+                SizedBox(
+                  width: 3.0,
+                ),
+                Text('ULTRAMAN DECKER'),
+              ],
+            ),
+            trailing: Text('18.00'),
+          ),
+        ],
+      ),
+    ),
+    floatingActionButton: FloatingActionButton(
+      backgroundColor: Colors.green,
+      onPressed: () {},
+      child: Icon(
+        Icons.chat_bubble,
+        color: Colors.white,
+      ),
+    ),
+  );
+}
