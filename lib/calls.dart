a8ed22abe73b760f40540fe94b30f62e8cd2c61b
FlutLab.io <>
2022-05-31 06:53:08 +0000

    Autosave

--- lib/calls.dart
+++ lib/calls.dart
@@ -55,6 +55,41 @@ class Calls extends StatelessWidget {
                 color: Colors.green,
               ),
             ),
+            ListTile(
+              leading: CircleAvatar(
+                backgroundImage: NetworkImage("https://i0.wp.com/www.cirebonkota.go.id/wp-content/uploads/2018/05/jokowi.jpg"),
+                radius: 30.0,
+              ),
+              title: Text(
+                'GUTS SELECT',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18.0,
+                ),
+              ),
+              subtitle: Row(
+                children: [
+                  Icon(
+                    Icons.south_west,
+                    color: Colors.green,
+                    size: 16.0,
+                  ),
+                  SizedBox(
+                    width: 3.0,
+                  ),
+                  Text(
+                    '24 menit yang lalu',
+                    style: TextStyle(
+                      fontSize: 16.0,
+                    ),
+                  ),
+                ],
+              ),
+              trailing: Icon(
+                Icons.videocam,
+                color: Colors.green,
+              ),
+            ),
           ],
         ),
       ),
